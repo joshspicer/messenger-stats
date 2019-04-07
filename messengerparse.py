5293c31e9a6fbd9ee4223fb43463a34a9c9d3c0f
Josh Spicer <hello@joshspicer.com>
2019-04-07 12:08:30 -0400

    Now counts individual reacts. Add gitignore for json files

--- messengerparse.py
+++ messengerparse.py
@@ -17,6 +17,7 @@ COUNT: Counts how many messages occur in a given timeframe.
 
 '''
 
+
 class MessengerParse:
 
     def __init__(self, input='', mode="COUNT"):
@@ -28,13 +29,15 @@ class MessengerParse:
         # "private" variables
         self.data = []
         self.participants = []
+        self.msgs = []
 
      # Declare cli arguments here
     def parse_args(self):
         """Parse"""
         self.parser.description = 'Messenger Bulk Data Parsing Tool'
         self.parser.add_argument('input', help="json messenger archive file")
-        self.parser.add_argument('--mode', help="Specify the mode to operate in. DEFAULT: MOST_POPULAR | Options: {0}".format(modesString))
+        self.parser.add_argument(
+            '--mode', help="Specify the mode to operate in. DEFAULT: MOST_POPULAR | Options: {0}".format(modesString))
         self.args = self.parser.parse_args()
 
         # Set all args here to class variables
@@ -50,17 +53,21 @@ class MessengerParse:
         # Get a dictionary of participants
         tmp = self.data['participants']
         for person in tmp:
-           self.participants.append(person['name'])
+            self.participants.append(person['name'])
+
+        # Map the messages dictionary to object
+        self.msgs = self.data['messages']
 
     def run(self):
         if self.mode == "COUNT":
-            return self.countInTimeFrame(1549972846000,1550059246000)
+            # TODO: allow for user input bounds
+            return self.countInTimeFrame(1549972846000, 1550059246000)
         elif self.mode == "MOST_POPULAR":
             return self.countMostPopularPerson()
         # ...more...
         else:
             return "Please supply a mode!"
-            
+
     def handleCLI(self):
         self.parse_args()
         self.setup_generic()
@@ -68,10 +75,9 @@ class MessengerParse:
 
     ## ==================== WORKERS ====================== ##
     def countInTimeFrame(self, start, end):
-        msgs = self.data['messages']
         count = 0
-        for m in msgs:
-            if m['timestamp_ms'] > start and  m['timestamp_ms'] < end:
+        for m in self.msgs:
+            if m['timestamp_ms'] > start and m['timestamp_ms'] < end:
                 count = count + 1
 
         return count
@@ -82,24 +88,38 @@ class MessengerParse:
         # Gen a dictionary with participants name
         popularityDict = {}
         for person in self.participants:
-            popularityDict[person] = 0
-        # Go though every message, and simple count the reacts on the message.
-        msgs = self.data['messages']
-        for m in msgs:
+            popularityDict[person] = {
+                "count": 0, "thumbsDown": 0, "heartEyes": 0, "cry": 0,
+                "laugh": 0, "wow": 0, "thumbsUp": 0, "angry": 0
+            }
+
+        # Go though every message, and simply count the reacts on the message.
+        for m in self.msgs:
             sender = m['sender_name']
-            count = 0
+
             if 'reactions' in m:
                 for r in m['reactions']:
-                    count = count + 1
-            popularityDict[sender] = popularityDict[sender] + count
+                    # Count this react regardless of its type.
+                    popularityDict[sender]['count'] = popularityDict[sender]['count'] + 1
+                    # Check different kinds of reacts
+                    if r['reaction'] == "\u00f0\u009f\u0091\u008e":  # Thumbs Down
+                        popularityDict[sender]['thumbsDown'] = popularityDict[sender]['thumbsDown'] + 1
+                    if r['reaction'] == "\u00f0\u009f\u0098\u008d":  # Heart Eyes
+                        popularityDict[sender]['heartEyes'] = popularityDict[sender]['heartEyes'] + 1
+                    if r['reaction'] == "\u00f0\u009f\u0098\u0086":  # Laugh React
+                        popularityDict[sender]['laugh'] = popularityDict[sender]['laugh'] + 1
+                    if r['reaction'] == "\u00f0\u009f\u0098\u00ae":  # Wow React
+                        popularityDict[sender]['wow'] = popularityDict[sender]['wow'] + 1
+                    if r['reaction'] == "\u00f0\u009f\u0091\u008d":  # Thumbs up
+                        popularityDict[sender]['thumbsUp'] = popularityDict[sender]['thumbsUp'] + 1
+                    if r['reaction'] == "\u00f0\u009f\u0098\u00a0":  # Angry React
+                        popularityDict[sender]['angry'] = popularityDict[sender]['angry'] + 1
+                    if r['reaction'] == "\u00f0\u009f\u0098\u00a2":  # Cry React
+                        popularityDict[sender]['cry'] = popularityDict[sender]['cry'] + 1
 
-        return sorted(popularityDict.items(), key=lambda x: x[1], reverse=True)
-
-
+        return sorted(popularityDict.items(), key=lambda x: x[1]['count'], reverse=True)
 
      ## =================================================== ##
-
-
 
 
 if __name__ == '__main__':
